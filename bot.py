bd3fb1a468825a053a1d3485c51bcbb3d6d6dad7
Myudio6 <mahdibah1379@gmail.com>
2025-04-28 16:41:12 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import telebot
+import random
+
+# توکن رباتت
+TOKEN = '7482224806:AAEIiBJVmH8uF3i83z_X1jomNcuMo5jxAVw'
+
+# استارت ربات
+bot = telebot.TeleBot(TOKEN)
+
+# لیست استیکرها
+stickers = [
+    'CAACAgUAAxkBAAEDq19lpsk1xyz',  # استیکر ۱
+    'CAACAgUAAxkBAAEDq2Blpsk7fgh',  # استیکر ۲
+    # اینجا استیکرهای بیشتری بعداً میتونی اضافه کنی
+]
+
+# واکنش به هر پیامی
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    text = message.text.lower()
+    
+    if "بغل" in text:
+        bot.send_sticker(message.chat.id, random.choice(stickers))
+        bot.send_message(message.chat.id, "محکم بغلت کردم عشقم!")
+    elif "بوس" in text:
+        bot.send_sticker(message.chat.id, random.choice(stickers))
+        bot.send_message(message.chat.id, "یه بوس خوشمزه برات فرستادم!")
+    else:
+        bot.send_message(message.chat.id, "دوست دارم قند عسلم!")
+
+# اجرای ربات
+bot.polling()
